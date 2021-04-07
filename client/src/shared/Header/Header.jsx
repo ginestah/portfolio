0ca36f63c7265b62da3aceafaf6b03ec82b1b237
ginestah <68362184+ginestah@users.noreply.github.com>
2021-04-07 18:37:51 -0400

    added more styling to page

--- client/src/shared/Header/Header.jsx
+++ client/src/shared/Header/Header.jsx
@@ -3,13 +3,31 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="header">
-      <img />
+      {/* <img /> */}
       <nav className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/about">About Me</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/contact">Contact</Link>
-        <a href="">Resume</a>
+        <Link className="logo" to="/">
+          Huckleberry Ginesta <br />
+          <div className="sub-heading"> Software Engineer</div>
+        </Link>{" "}
+        <div className="link-container">
+          <Link className="section-link" to="/about">
+            About Me
+          </Link>
+          <Link className="section-link" to="/projects">
+            Projects
+          </Link>
+          <Link className="section-link" to="/contact">
+            Contact
+          </Link>
+          <a
+            href="https://drive.google.com/file/d/1n1z9XwrZ4RM0-jVsO0Ek_uaEAHgT8MHf/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+            className="section-link"
+          >
+            Resume
+          </a>
+        </div>
       </nav>
     </header>
   );
