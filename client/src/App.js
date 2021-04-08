1cdae41adfe40e15b1ac690af0eb5405e5613172
ginestah <68362184+ginestah@users.noreply.github.com>
2021-04-07 20:24:04 -0400

    added more styling, working on transitions

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Layout from "./shared/Layout/Layout";
+import { Route } from "react-router-dom";
+import { useState } from "react";
+import AboutMe from "./screens/AboutMe";
+import { Transition } from "react-transition-group";
 
 function App() {
   return (
     <div className="App">
-      <Layout></Layout>
+      <Layout>
+        <Route exact path="/about" component={AboutMe} />
+      </Layout>
     </div>
   );
 }
---
+++ client/src/screens/AboutMe.jsx
@@ -0,0 +1,10 @@
+import { Transition } from "react-transition-group";
+import Layout from "../shared/Layout/Layout";
+
+export default function AboutMe() {
+  return (
+    <Layout>
+      <div className="heck">I am a fade transition</div>
+    </Layout>
+  );
+}
