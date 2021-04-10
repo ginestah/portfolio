f8e5bc02337621276a4819769ae233e29b022b84
ginestah <68362184+ginestah@users.noreply.github.com>
2021-04-10 10:41:58 -0400

    changed colors on theme border

--- client/src/styles/GlobalStyles.js
+++ client/src/styles/GlobalStyles.js
@@ -5,5 +5,7 @@ export const GlobalStyles = createGlobalStyle`
     color: ${({ theme }) => theme.text};
     transition: all 0.50s linear;
   }
- 
+  .project-card{
+    border: 1px solid ${({ theme }) => theme.text};
+  }
   `;
