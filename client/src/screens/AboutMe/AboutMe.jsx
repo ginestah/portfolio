586f0277c6ef58260dc307e32b306e81dd22f239
ginestah <68362184+ginestah@users.noreply.github.com>
2021-04-16 10:58:40 -0400

    nice

--- client/src/screens/AboutMe/AboutMe.jsx
+++ client/src/screens/AboutMe/AboutMe.jsx
@@ -9,32 +9,25 @@ export default function AboutMe() {
         alt="headshot"
       />
       <h1 className="title">Hey! My name is Huck.</h1>
-      <h2 className="sub-title">
-        I am a Software Engineer and Climbing Instructor.
-      </h2>
+      <h2 className="sub-title">I am a Software Engineer</h2>
 
       <main className="about-me">
-        With a B.S. in Conservation Biology and a background in climbing
-        instruction, I bring a detail-oriented, driven, and communicative
-        personality to the table. I leverage my experience in teaching and
-        science to break down information, teach complex ideas and effectively
-        test and troubleshoot.
+        My Stack: React, Javascript, Ruby, Ruby on Rails, Express, HTML, CSS,
+        MongoDB, PostgreSQL, Jest, and Rspec.
         <br />
         <br />I am looking for a position with a environmentally conscious
         business with a fun engaging culture. I have an eagerness to learn new
         tech, and the proven ability to do so effectively, and quickly. At
         General Assemblies 12 week Software Engineering Immersive program I was
-        able to learn a multitude of tech stacks, as well as some fundamentals
-        of data structures and algorithms. The tech I am most eager to work with
-        and enjoy using currently, includes:
-        <ul>
-          <li>Javascript</li>
-          <li>Ruby</li>
-          <li>HTML/CSS</li>
-          <li>React.js</li>
-          <li>Ruby on Rails</li>
-          <li>Express.js</li>
-        </ul>
+        able to learn a multitude of tech stacks, as well as data structure and
+        algorithm fundamentals.
+        <br />
+        <br /> With a B.S. in Conservation Biology and a background in climbing
+        instruction, I bring a detail-oriented, driven, and communicative
+        personality to the table. I leverage my experience in teaching and
+        science to break down information, teach complex ideas and effectively
+        test and troubleshoot.
+        <br />
       </main>
     </div>
   );
